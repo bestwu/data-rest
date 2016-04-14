65b18010320750dab6c51d7795f51ea1aa2d91e9
Peter <piterwu@outlook.com>
2016-04-14 13:56:33 +0800

    增加BaseController 方法

--- core/src/main/java/cn/bestwu/framework/rest/controller/BaseController.java
+++ core/src/main/java/cn/bestwu/framework/rest/controller/BaseController.java
@@ -2,6 +2,8 @@ package cn.bestwu.framework.rest.controller;
 
 import cn.bestwu.framework.event.DefaultSortEvent;
 import cn.bestwu.framework.rest.support.Response;
+import cn.bestwu.framework.util.ParameterUtil;
+import cn.bestwu.framework.util.ResourceUtil;
 import cn.bestwu.framework.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,12 +12,18 @@ import org.springframework.context.MessageSource;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.mapping.PersistentEntity;
+import org.springframework.data.repository.support.RepositoryInvoker;
+import org.springframework.data.repository.support.RepositoryInvokerFactory;
+import org.springframework.util.ClassUtils;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 /**
  * 基础控制器
@@ -46,6 +54,9 @@ public abstract class BaseController extends Response {
 
 	@Autowired(required = false)
 	protected HttpServletRequest request;
+
+	@Autowired
+	private RepositoryInvokerFactory invokerFactory;
 
 	/**
 	 * 得到国际化信息 未找到时返回代码 code
@@ -109,4 +120,49 @@ public abstract class BaseController extends Response {
 		}
 		return pageable;
 	}
+
+	protected boolean equalsVersion(String version) {
+		return ResourceUtil.equalsVersion(request, version);
+	}
+
+	/**
+	 * @param key 参数名称
+	 * @return 是否存在此参数，此方法在request body方式提交数据时可能无效
+	 */
+	protected boolean hasParameter(String key) {
+		return ParameterUtil.hasParameter(request.getParameterMap(), key);
+	}
+
+	protected boolean hasParameterKey(String key) {
+		return ParameterUtil.hasParameterKey(request.getParameterMap(), key);
+	}
+
+	/**
+	 * 设置已存在的相关资源
+	 *
+	 * @param source source
+	 * @param action 操作
+	 * @param <S>    S
+	 */
+	protected <S> void setRelatedProperty(S source, Consumer<S> action) {
+		if (source != null) {
+			Class<?> sourceClass = source.getClass();
+			if (ClassUtils.isCglibProxy(source)) {
+				sourceClass = sourceClass.getSuperclass();
+			}
+			PersistentEntity<?, ?> sourceEntity = getPersistentEntity(sourceClass);
+			Object id = sourceEntity.getIdentifierAccessor(source).getIdentifier();
+			if (id != null) {
+				RepositoryInvoker repositoryInvoker = invokerFactory.getInvokerFor(sourceClass);
+				source = repositoryInvoker.invokeFindOne((Serializable) id);
+				if (source == null) {
+					throw new IllegalArgumentException(getText("id.notFound"));
+				}
+			} else {
+				source = null;
+			}
+		}
+		action.accept(source);
+	}
+
 }
